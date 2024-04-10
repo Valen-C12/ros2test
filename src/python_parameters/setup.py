c1638e68d2a7bfe6995f6ac1ee1ce66528944d52
Valen <valen@valen.wang>
2024-04-10 15:19:54 +0800

    feat: add entry

--- src/python_parameters/setup.py
+++ src/python_parameters/setup.py
@@ -1,25 +1,25 @@
 from setuptools import find_packages, setup
 
-package_name = 'python_parameters'
+package_name = "python_parameters"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='valen',
-    maintainer_email='valen@valen.wang',
-    description='Python parameter tutorial',
-    license='MIT',
-    tests_require=['pytest'],
+    maintainer="valen",
+    maintainer_email="valen@valen.wang",
+    description="Python parameter tutorial",
+    license="MIT",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
+        "console_scripts": [
+            "minimal_param_node = python_parameters.python_parameters_node:main",
         ],
     },
 )
