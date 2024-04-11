069a4aa547c94821420adccc15ac6bf3aca6e47f
Valen <valen@valen.wang>
2024-04-11 10:42:53 +0800

    feat: update setup file to add launch and config files

--- src/py_launch/setup.py
+++ src/py_launch/setup.py
@@ -1,25 +1,33 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
-package_name = 'py_launch'
+package_name = "py_launch"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (
+            os.path.join("share", package_name, "launch"),
+            glob(os.path.join("launch", "*launch.[pxy][yma]*")),
+        ),
+        (
+            os.path.join("share", package_name, "config"),
+            glob(os.path.join("config", "*.yaml")),
+        ),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='valen',
-    maintainer_email='valen@valen.wang',
-    description='Python launch files',
-    license='MIT',
-    tests_require=['pytest'],
+    maintainer="valen",
+    maintainer_email="valen@valen.wang",
+    description="Python launch files",
+    license="MIT",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-        ],
+        "console_scripts": [],
     },
 )
